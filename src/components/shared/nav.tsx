54d7b87e9ad64c379e89b3c20cdc6f8867ffae24
Patrik <patriklevak@gmail.com>
2024-04-17 12:37:29 +0200

    navbar little fix

--- src/components/shared/nav.tsx
+++ src/components/shared/nav.tsx
@@ -6,7 +6,7 @@ import { Code } from "lucide-react";
 export default function Nav() {
   return (
     <>
-      <div className="fixed z-[998] inset-0 bottom-auto h-32 bg-gradient-to-b from-background via-background to-transparent" />
+      <div className="fixed z-[998] inset-0 bottom-auto dark:h-32 h-20 bg-gradient-to-b from-background via-background to-transparent" />
       <nav className="bg-card border border-border z-[999] rounded-lg fixed mx-auto inset-3 bottom-auto max-w-5xl px-4 py-2 dark:shadow-white/5 shadow-black/10 shadow-2xl flex items-center justify-between">
         <Code className="size-6" />
         <ul className="flex items-center gap-8 ml-2 max-md:hidden">
